286d0297a9977ee894ae7b374ef8b0c12354d499
jimmcgaw <jpmcgaw@gmail.com>
2016-06-06 14:36:46 -0700

    update wsgi file

--- journal/views.py
+++ journal/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Article
 from .forms import ArticleForm
 
@@ -8,10 +9,12 @@ import datetime
 def index(request):
     return render(request, 'index.html')
 
+@login_required
 def account(request):
     user_articles = Article.objects.filter(author=request.user)
     return render(request, 'account.html', locals())
 
+@login_required
 def new_article(request):
     if request.method == 'POST':
         article_data = request.POST.copy()
@@ -27,7 +30,21 @@ def new_article(request):
         article = Article(author=request.user)
         form = ArticleForm(instance=article)
 
-    return render(request, 'new_article.html', locals())
+    return render(request, 'article_form.html', locals())
+
+@login_required
+def edit_article(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    form = Article(instance=article)
+    if request.method == 'POST':
+        article_data = request.POST.copy()
+        article_data['author_id'] = request.user.id
+        article_data['published_at'] = datetime.datetime.now()
+        article_data['is_published'] = True
+        # and so on
+    return render(request, 'article_form.html', locals())
+
+
 
 def view_article(request, slug):
     article = get_object_or_404(Article, slug=slug)
--- stemblog/wsgi.py
+++ stemblog/wsgi.py
@@ -8,6 +8,19 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import site
+
+site.addsitedir('~/.virtualenvs/stemblog/local/lib/python2.7/site-packages')
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
+sys.path.append(parent_dir)
+
+# Activate your virtual env
+try:
+    activate_env = os.path.expanduser("/home/jim/.virtualenvs/stemblog/bin/activate_this.py")
+    execfile(activate_env, dict(__file__=activate_env))
+except IOError:
+    pass
 
 from django.core.wsgi import get_wsgi_application
 
