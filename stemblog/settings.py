e023ec34f6bd28666be5a0280304ba73150e136d
jimmcgaw <jpmcgaw@gmail.com>
2016-06-19 15:08:25 -0700

    add gravatar

--- stemblog/settings.py
+++ stemblog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'journal',
     'registration',
+    'django_gravatar',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -122,7 +123,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
