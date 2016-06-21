6275eada50c6df69bc8a1011722eb8c01c8d8de7
jimmcgaw <jpmcgaw@gmail.com>
2016-06-20 18:09:40 -0700

    rework journal user-logged-in page layout

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,13 @@ var sass = require('gulp-sass');
 gulp.task('default', ['sass:watch']);
 
 gulp.task('sass', function () {
-  return gulp.src('./media/sass/styles.scss')
+  return gulp.src(['./media/sass/styles.scss', './media/sass/index.scss'])
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./media/css'));
 });
 
 gulp.task('sass:watch', function () {
   gulp.watch('./public/sass/**/*.scss', ['sass', 'inject']);
-});
-
-gulp.task('jshint', function(){
-   return gulp.src(jsFiles)
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish', {
-       verbose: true
-   }))
-   .pipe(jscs());
 });
 
 // \Big[\frac{\hbar^2}{2m}\frac{\partial^2}{\partial x^2} + V\Big]\Psi = i\hbar \frac{\partial}{\partial t} \Psi
@@ -34,7 +25,7 @@ gulp.task('inject', function(){
       './static/folio/css/**/*.css',
       './static/css/**/*.css',
     ], {read: false});
-    
+
     var injectOptions = {
         ignorePath: '/'
     };
